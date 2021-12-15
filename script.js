cabb1ee2c30ac802ccc1d791b5a188f61bce5457
Tirta Styadi <tirta.setiady@yahoo.com>
2021-12-15 19:42:17 +1100

    ADD: Adding array

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Array
+
+var lowerCase = Array.from("abcdefghijklmnopqrstuvwxyz");
+var upperCase = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var numeric = Array.from("1234567890");
+var symbol = Array.from(".,<>/?;:[{]}\|=+-_)(*&^%$#@!~`");
+
+//RANDOM FUNCTION
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
