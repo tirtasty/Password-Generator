4be4785e26c2a54cd92875d3980a92e9a83c3f3a
Tirta Styadi <tirta.setiady@yahoo.com>
2021-12-16 07:22:26 +1100

    ADD: Adding function for each random character

--- script.js
+++ script.js
@@ -9,6 +9,36 @@ var numeric = Array.from("1234567890");
 var symbol = Array.from(".,<>/?;:[{]}\|=+-_)(*&^%$#@!~`");
 
 //RANDOM FUNCTION
+function randomLower(){
+  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
+function randomUpper(){
+  return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+function randomNumber(){
+  return numeric[Math.floor(Math.random() * numeric.length)];
+}
+function randomSymbol(){
+  return symbol[Math.floor(Math.random() * symbol.length)];
+}
+
+//CHECKLIST
+
+function optionChar(){
+  var askCharacter = window.prompt("Please, input charcter between 8 - 128");
+  if (askCharacter < 8){
+    window.alert("Minimum Character is 8 !");
+  }
+  else if (askCharacter > 128){
+    window.alert("Maximum Character is 128 !");
+  }
+  var askLower = window.confirm("Include lowercase letter?");
+  var askUpper = window.confirm("Include uppercase letter?");
+  var askSymbol = window.confirm("Include symbol?");
+  var askNumber = window.confirm("Include number?");
+}
+
+optionChar();
 
 
 
