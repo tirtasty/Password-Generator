347276589d755717543912a9d701543ed6f0a713
Tirta Styadi <tirta.setiady@yahoo.com>
2021-12-18 13:25:06 +1100

    cleaning up for unncessary code

--- script.js
+++ script.js
@@ -8,36 +8,6 @@ var upperCase = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 var numeric = Array.from("1234567890");
 var symbol = Array.from(".,<>/?;:[{]}\|=+-_)(*&^%$#@!~`");
 var storePassword = [];
-var storePassword = storePassword.concat(lowerCase)
-
-
-//RANDOM FUNCTION
-function randomLower(){
-  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
-}
-function randomUpper(){
-  return upperCase[Math.floor(Math.random() * upperCase.length)];
-}
-function randomNumber(){
-  return numeric[Math.floor(Math.random() * numeric.length)];
-}
-function randomSymbol(){
-  return symbol[Math.floor(Math.random() * symbol.length)];
-}
-
-//OBJECT RANDOM
-var randomFunc = {
-  lower: randomLower(),
-  upper: randomUpper(),
-  symbol: randomSymbol(),
-  number: randomNumber()
-};
-
-// CHECK IF FUNCTION IS WORKING
-console.log(randomLower());
-console.log(randomUpper());
-console.log(randomSymbol());
-console.log(randomNumber());
 
 //CHECKLIST
 
@@ -72,31 +42,13 @@ if (askSymbol === true){
 console.log(storePassword);
 
 
-//ARRAY OF OPTION
-var arrayQuestion = {
-  askLower:askLower,
-  askUpper:askUpper,
-  askNumber:askNumber,
-  askSymbol:askSymbol
-
-}
-console.log(arrayQuestion);
-
-
-
 var generatePass = [];
-var result = generatePass.join('')
-console.log("result:", result)
 //LOOPING TO GET PASSWORD
 for (var i = 0; i < characterAmount ; i ++){
   
   var random = storePassword[Math.floor(Math.random() * storePassword.length)]
   generatePass.push(random)
-  
-  console.log("store :", random)
 }
-console.log(generatePass.join(''))
-
 
 // Write password to the #password input
 function writePassword() {
