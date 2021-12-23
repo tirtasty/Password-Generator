ebd65513e3da528eb4c98d59c7e72f574d9aacc7
Tirta Styadi <tirta.setiady@yahoo.com>
2021-12-23 22:42:16 +1100

    FIX: fixing the function

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+var resetButton = document.getElementById("reset")
+
 
 //Array
 
-var lowerCase = Array.from("abcdefghijklmnopqrstuvwxyz");
-var upperCase = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var numeric = Array.from("1234567890");
-var symbol = Array.from(".,<>/?;:[{]}\|=+-_)(*&^%$#@!~`");
-var storePassword = [];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "12345678901234567890"
+var symbol = ".<>/?;:[{]}\|=+-_)(*&^%$#@!~`"
+var generatePass = [];
 
 //CHECKLIST
 
-var characterAmount = window.prompt("Please, input character between 8 - 128");
+function generatePassword(){
+  var characterAmount = window.prompt("Please, input character between 8 - 128");
   if (characterAmount < 8 || characterAmount > 128 ){
     window.alert("Number is Invalid");
     characterAmount = 8;
   };
 //CHECK THE VALUE EITHER STRING OR NUMBER
-console.log("value" , characterAmount)
+  console.log("value" , characterAmount)
 
-var askLower = window.confirm("Include lowercase letter?");
-var askUpper = window.confirm("Include uppercase letter?");
-var askSymbol = window.confirm("Include symbol?");
-var askNumber = window.confirm("Include number?");
+  var askLower = window.confirm("Include lowercase letter?");
+  var askUpper = window.confirm("Include uppercase letter?");
+  var askSymbol = window.confirm("Include symbol?");
+  var askNumber = window.confirm("Include number?");
 
 //FILTER FOR PASSWORD
-if (askLower === true){
-  storePassword = storePassword.concat(lowerCase)
+  var storePassword = "";
+  if (askLower){
+  storePassword = storePassword + lowerCase;
 }
-if (askUpper === true){
-  storePassword = storePassword.concat(upperCase)
+  if (askUpper){
+  storePassword = storePassword + upperCase;
 }
-if (askNumber === true){
-  storePassword = storePassword.concat(numeric)
+  if (askNumber){
+  storePassword = storePassword + numeric;
 }
-if (askSymbol === true){
-  storePassword = storePassword.concat(symbol)
+  if (askSymbol){
+  storePassword = storePassword + symbol;
 }
+  //CONCAT FILTER CHECKING
+  console.log(storePassword); 
 
-//CONCAT FILTER CHECKING
-console.log(storePassword);
-
-
-var generatePass = [];
+  ;
 //LOOPING TO GET PASSWORD
-for (var i = 0; i < characterAmount ; i ++){
-  
-  var random = storePassword[Math.floor(Math.random() * storePassword.length)]
-  generatePass.push(random)
+  for (var i = 0; i < characterAmount ; i ++){
+    var random = storePassword[Math.floor(Math.random() * storePassword.length)]
+    generatePass.push(random);
+    console.log(random)
+    var password = generatePass.join('')
+    passwordText.value = password;
 }
+};
+
+function clearArea(){
+  passwordText.value = "";
+}
+
+resetButton.addEventListener('click', clearArea, false)
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePass.join('')
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  clearArea();                     
+  generatePassword();
+  
+  
 
 }
 
